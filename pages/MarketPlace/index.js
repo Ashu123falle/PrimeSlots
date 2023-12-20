aa524de7ef16c5323fa2f13522619440614c3253
Eswar <eswarnarayana797@gmail.com>
2023-12-20 23:48:46 +0530

    Added filter component for marketplace page with some dummy data

---
+++ pages/MarketPlace/index.js
@@ -0,0 +1,21 @@
+// pages/index.js
+import React from 'react';
+import FilterComponent from '../../Components/FilterComponent';
+
+// Example data
+const data = [
+  { title: 'Result 1', state: 'Andhra Pradesh', category: 'Category 1', filter: 'Filter 1' },
+  { title: 'Result 2', state: 'Assam', category: 'Category 2', filter: 'Filter 2' },
+  { title: 'Result 3', state: 'Arunachal Pradesh', category: 'Category 1', filter: 'Filter 3' },
+  // Add more data as needed
+];
+
+const Home = () => {
+  return (
+    <div>
+      <FilterComponent data={data} />
+    </div>
+  );
+};
+
+export default Home;
