c38cf23083c6cad39080d9b2180ff03131c4bdfd
Umesh <umeshkhantwal@nexocide.com>
2023-12-20 23:25:23 -0800

    created search result page

--- pages/SearchResultPage/index.js
+++ pages/SearchResultPage/index.js
@@ -1,3 +1,5 @@
+import Footer from '@/Components/Footer'
+import BlogCard from '@/Components/blogCard'
 import React from 'react'
 
 const SearchResultPage = () => {
@@ -7,8 +9,10 @@ const SearchResultPage = () => {
                 <div>
                     <h1>Home/MarketPlace/Billboards</h1>
                     <hr />
-                    <h2>Showing 1-20 out of 2356 Products</h2>
+                    <h2 className='text-right'>Showing 1-20 out of 2356 Products</h2>
                 </div>
+                <BlogCard/>
+                <Footer/>
             </div>
         </>
     )
