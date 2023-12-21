6c090bd103ce56fa7ad118555a81f4e9ea3047aa
Umesh <umeshkhantwal@nexocide.com>
2023-12-21 03:56:57 -0800

    update in searchResultPage

--- Components/blogCard/index.js
+++ Components/blogCard/index.js
@@ -10,10 +10,10 @@ export default function BlogCard({ searchData }) {
                     searchData.map((curElem) => {
                         return (
                             <>
-                                <li className="list-none rounded-xl hover:shadow-lg w-[280px] h-[400px] group-hover:h-[400px] shadow
+                                <li className="list-none rounded-xl hover:shadow-lg w-[280px] h-[420px] group-hover:h-[400px] shadow
                                 bg-white rounded-lg shadow border border-slate-300 flex-col justify-start 
                                 items-start inline-flex m-3" key={curElem.id}>
-                                    <img src="https://s3-alpha-sig.figma.com/img/2fd8/7a45/64dd253446572cc2498028120d988e77?Expires=1704067200&Signature=ClDbx4AeUrl63d0~X8wzIyV6wejLfiKrF6jVs5-oS4~U7tYDby0v7L98lDKMWSZ51pagAcOAUFLMyrWhcWsZkroDR6mFUMS~LxyRq6uKg6fdRM67kQ4Y52z6Ekoe~y1hROyYeqpn~UWj3JXN3~nWIncU~yFUD1wl~SLb126-He9WvAW8aT5rqiIqkF5Y9ozREYNZISw2gic9SYm39ASIDSoAyFJpb-Zr71WRvoVWFbmsRfZeu~u0PmYh6O~3G8wNMy-3iCnY5RcvY2a744V~ngfJFMcnwqoLY~k~DOineq82q~XKr7ASt53fgN~I2tvjy2c5PMbfiY~3SU~95YZHuw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                                    <img src={curElem.image}
                                         className="h-[135px] w-[100%] rounded-t-xl"
 
                                     />
--- pages/SearchResultPage/index.js
+++ pages/SearchResultPage/index.js
@@ -12,7 +12,7 @@ const SearchResultPage = () => {
             <div className='my-5'>
                 <div>
                     <h1 className='py-4 px-40'><span className='text-gray-400'> Home / MarketPlace /</span> <span className='text-black font-semibold'>Billboards</span> </h1>
-                    <hr className='bg-black' />
+                    <hr className='border border-gray-300' />
                     <h2 className='text-right py-3 pr-52'><span className='font-semibold'>Showing 1-20</span> <span className='text-gray-400'>out of 2356 Products</span></h2>
                 </div>
                 <BlogCard searchData = {searchData}/>        
