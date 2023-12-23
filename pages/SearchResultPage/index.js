b57ab15e2573945fe3bfedb2731905a6f6611c8c
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2023-12-23 17:12:14 +0530

    fixed PeopleAlsoLook component

--- pages/SearchResultPage/index.js
+++ pages/SearchResultPage/index.js
@@ -12,18 +12,19 @@ const SearchResultPage = () => {
     
 
     return (
-        <div className="">
+        <>
+        <Navbar/>
             <div>
                    <h1 className='px-5 py-4 mb-[-10px] text-[14px]'><span className='text-gray-400 '> Home / MarketPlace /</span> <span className='text-black font-semibold'>Billboards</span> </h1>
                    <hr className='border border-gray-300 w-[100vw]' />
                    <h2 className='text-right py-3 text-[14px] px-5'><span className='font-semibold'>Showing 1-20</span> <span className='text-gray-400'>out of 2356 Products</span></h2>
              </div>
 
-        <div className="px-5 md:px-8 pb-5">
-             <ul className='flex flex-wrap h-[90vh] overflow-y-auto w-full'>
+        <div className="px-5 md:px-8 pb-6">
+             <ul className='flex flex-wrap h-[120vh] overflow-y-auto w-full'>
                    {
                        searchData.map((curElem, ind) => {
-                           return <BlogCard key={ind} id={curElem.id} image={curElem.image} heading={curElem.heading} name={curElem.name} description1={curElem.description1} description2={curElem.description2} description3={curElem.description3} description4={curElem.description4} />
+                           return <BlogCard key={ind} data={curElem} />
                        })
                    }
                </ul>
@@ -33,12 +34,14 @@ const SearchResultPage = () => {
 
             <div className="pt-2">
             <h1 className='text-2xl md:text-3xl font-bold mb-5'>People also looked for</h1>
-                
-                <PeopleAlsoLookFor/>
+
+    
+            <PeopleAlsoLookFor/>
+      
             </div>
         </div>
-
-        </div>
+        <Footer/>
+        </>
     )
 }
 
