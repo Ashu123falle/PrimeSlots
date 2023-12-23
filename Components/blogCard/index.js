27d006f6e97b1c23e1f587c4ce9f10f1aa031e0a
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2023-12-23 17:11:24 +0530

    Created PeopleAlsoLookFor component

--- Components/blogCard/index.js
+++ Components/blogCard/index.js
@@ -2,13 +2,15 @@ import wishlist from '@/pages/assets/Wishlist.svg'
 import Image from 'next/image'
 
 export default function BlogCard(props) {
+    const {data} = props
+    const {image,heading,name,description1,description2,description3,description4} = data
+
     return (
         
-            <li className="list-none rounded-xl w-11/12 h-[400px]  max-w-[400px] sm:w-[300px] bg-white shadow border border-slate-300 
+            <li className="list-none rounded-xl w-11/12 h-[400px] shrink-0 max-w-[400px] sm:w-[300px] bg-white shadow border border-slate-300 
                 flex-col justify-start items-start inline-flex m-2 group hover:shadow-2xl  hover:h-[390px]
-                " 
-                key={props.id} > 
-                <img src={props.image}
+                "> 
+                <img src={image}
                     className="h-[180px] w-[100%] rounded-t-xl"
                 />
                 <span className="self-end mt-[-20px] bg-white rounded-full w-10 h-10 flex justify-center items-center mr-4">
@@ -17,19 +19,19 @@ export default function BlogCard(props) {
                 <div className="px-6 pt-4 pb-2  flex flex-col w-[100%] justify-between h-[50%]">
                     <div className="ml-[-6px] h-[21.98px] px-[10px] py-[1.57px] bg-stone-100 rounded-[39.26px] justify-start 
                  gap-[4.71px] items-start self-start">
-                        <h1 className="text-amber-500 text-sm font-normal font-['Figtree'] leading-tight">{props.heading}</h1>
+                        <h1 className="text-amber-500 text-sm font-normal font-['Figtree'] leading-tight">{heading}</h1>
                     </div>
-                    <p className="pt-1 text-neutral-950 text-lg font-normal font-['Figtree'] leading-tight">{props.name}</p>
+                    <p className="pt-1 text-neutral-950 text-lg font-normal font-['Figtree'] leading-tight">{name}</p>
 
                     <ul className=" pt-[10px] p-2 flex flex-wrap m-[-10px] justify-between">
                         <li className="h-[25px]  p-2 flex items-center bg-indigo-100 bg-opacity-60 rounded 
-                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tight my-1">{props.description1}</li>
+                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tight my-1">{description1}</li>
                         <li className="h-[25px]  p-2 flex items-center bg-indigo-100 bg-opacity-60 rounded 
-                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tight my-1">{props.description2}</li>
+                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tight my-1">{description2}</li>
                         <li className="h-[25px] p-2 flex items-center bg-indigo-100 bg-opacity-60 rounded 
-                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tight my-1">{props.description3}</li>
+                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tight my-1">{description3}</li>
                         <li className="h-[25px] p-2 flex items-center bg-indigo-100 bg-opacity-60 rounded 
-                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tightmy-1 ">{props.description4}</li>
+                text-indigo-600 text-sm font-normal font-['Figtree'] leading-tightmy-1 ">{description4}</li>
                     </ul>
 
                 <button className="w-[90px] h-[35px] mt-2 mb-2 rounded-[5px] border border-indigo-500 group-hover:bg-blue-500 group-hover:text-white bg-transparent
