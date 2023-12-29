db4eee0e339449b199e1a1c0317f5e387bbcbb35
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2023-12-29 09:28:55 +0530

    gave transitions on hovering for category cards.

--- components/CategoryCard/index.js
+++ components/CategoryCard/index.js
@@ -6,15 +6,18 @@ export const CategoryCard = (props) => {
     return (
       <li
         className=
-          "mx-3 bg-[#ffffff] flex-col items-center justify-center text-center list-none rounded-lg border-solid border-[rgba(170,170,170,0.30)] border shrink-0 w-[170px] h-[152px] relative overflow-hidden shadow hover:shadow-sm hover:font-bold group"
+          "mx-3 bg-[#ffffff] flex-col items-center justify-center text-center list-none rounded-lg border-solid border-[rgba(170,170,170,0.30)]border shrink-0 w-[170px] h-[152px] relative overflow-hidden shadow hover:shadow-sm hover:font-bold group transition-transform duration-300 transform-gpu hover:scale-105"
         
       >
+         <Link href={`/MediaListing?category=${encodeURIComponent(title)}`}>
          <img
           className="w-[170px] h-[104px] top-0"
           src={imageUrl}
           alt={alt}
         />
-        <Link href={`/MediaListing?category=${encodeURIComponent(title)}`}> <h1 className="group-hover:font-bold group-hover: text-[#224757] text-capstext text-center mt-3 font-['Figtree-Regular',_sans-serif] text-sm text-base leading-5 font-normal bottom-[10px]">
+       
+           <h1 className="group-hover:font-bold group-hover: text-[#224757] text-capstext text-center 
+           mt-3 font-['Figtree-Regular',_sans-serif] text-sm text-base leading-5 font-normal bottom-[10px]">
           {title}
         </h1>
         </Link>
