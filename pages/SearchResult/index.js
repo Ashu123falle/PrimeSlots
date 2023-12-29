b72abe21325e75ff96d65b3fbbc9f9cc441d4f87
jyothiswaroop077 <swaroopvallamkonda@nexocide.com>
2023-12-29 09:18:55 +0530

    alignments

--- pages/SearchResult/index.js
+++ pages/SearchResult/index.js
@@ -38,13 +38,13 @@ class SearchResultPage extends Component {
         <div className='box-border'>
             <Navbar/>
             <div>
-                   <h1 className='px-5 py-4 mb-[-10px] text-[14px]'><span className='text-gray-400 '> Home / MarketPlace /</span> <span className='text-black font-semibold'>Billboards</span> </h1>
+                   <h1 className='px-5 py-4 mb-[-10px] text-[14px] md:pl-16 md:mx-16'><span className='text-gray-400 '> Home / MarketPlace /</span> <span className='text-black font-semibold'>Billboards</span> </h1>
                    <hr className='border border-gray-300 w-[100vw]' />
-                   <h2 className='text-right py-3 text-[14px] px-5'><span className='font-semibold'>Showing 1-20</span> <span className='text-gray-400'>out of 2356 Products</span></h2>
+                   <h2 className='text-right py-3 text-[14px] px-5 md: pr-16 md:mr-16'><span className='font-semibold'>Showing 1-20</span> <span className='text-gray-400'>out of 2356 Products</span></h2>
              </div>
 
-        <div className="px-5 md:px-8 xl:px-16 pb-6">
-             <ul className='flex flex-wrap justify-around h-[120vh] overflow-y-auto w-full'>
+        <div className="px-5 md:px-1 lg:px-1 xl:px-16 pb-2">
+             <ul className='flex flex-wrap justify-center items-start h-[120vh] overflow-y-auto w-full'>
                    {
                        searchData.map((curElem, ind) => {
                            return <BlogCard key={ind} data={curElem} onClickWishlistButton = {this.onClickWishlistButton}/>
