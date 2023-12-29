d45133fda119e60aeadcf5c1e31cfc019289d3e6
Ashufalle123 <ashutoshfalle@nexocide.com>
2023-12-29 12:53:44 +0530

    adjusted size of logos and made testomonials a bit responsive

--- Components/JoinTheBrand/index.js
+++ Components/JoinTheBrand/index.js
@@ -4,7 +4,7 @@ import medium from '@/pages/assets/medium.svg'
 import google from '@/pages/assets/google.jpg'
 import grab from '@/pages/assets/grab.png'
 import microsoft from '@/pages/assets/microsoft.jpg'
-import  spotify from '@/pages/assets/spotify.jpg'
+import  spotify from '@/pages/assets/spotify.svg'
 import stripe from '@/pages/assets/stripe.png'
 import uber from '@/pages/assets/uber.svg'
 import youtube from '@/pages/assets/youtube.jpg'
@@ -23,32 +23,32 @@ const JoinTheBrand  = () => {
       </div>
     </div>
     <div className="flex flex-wrap items-center justify-evenly">
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={spotify} width={160} height={50} />
+    <span className="  self-end mt-[1px]   flex justify-center items-center  ml-4">
+            <Image src={spotify} width={180} height={50} className="items-stretch pb-6 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={google} width={160} height={50} />
+    <span className="  self-end mt-[1px]   flex justify-center items-center  ml-4">
+            <Image src={google} width={160} height={50} className="object-cover object-center pb-6 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={stripe} width={160} height={50} />
+    <span className="  self-end mt-[1px]   flex justify-center items-center  ml-4">
+            <Image src={stripe} width={120} height={40} className="object-cover object-center pb-8 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={youtube} width={160} height={50} />
+    <span className="  self-end mt-[1px]    flex justify-center items-center  ml-4">
+            <Image src={youtube} width={160} height={50} className="object-cover object-center pb-6 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={microsoft} width={160} height={50} />
+    <span className="  self-end mt-[1px]    flex justify-center items-center  ml-4">
+            <Image src={microsoft} width={160} height={50} className="object-cover object-center pb-8 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={medium} width={160} height={50} />
+    <span className="  self-end mt-[1px]    flex justify-center items-center  ml-4">
+            <Image src={medium} width={170} height={50} className="object-cover object-center mb-4 pb-14" />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={uber} width={160} height={50} />
+    <span className="  self-end mt-[1px]    flex justify-center items-center  ml-4">
+            <Image src={uber} width={110} height={50} className="object-cover object-center pb-8 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={zoom} width={160} height={50} />
+    <span className="  self-end mt-[1px]    flex justify-center items-center  ml-4">
+            <Image src={zoom} width={140} height={50} className="object-cover object-center pb-10 mb-4 " />
                 </span>
-    <span className="  self-end mt-[1px] border-black   flex justify-center items-center  ml-4">
-            <Image src={grab} width={160} height={50} />
+    <span className="  self-end mt-[1px]    flex justify-center items-center  ml-4">
+            <Image src={grab} width={120} height={50} className="object-cover object-center pb-10 mb-4 " />
                 </span>
     </div>
     </div>
--- Components/Testomonials/index.js
+++ Components/Testomonials/index.js
@@ -6,7 +6,7 @@ import user3 from '@/pages/assets/user3.png'
 function Testomonials() {
   return (
     <div className="flex flex-col items-center px-5">
-      <div className="text-blue-950 text-4xl whitespace-nowrap">
+      <div className="text-4xl text-blue-950 whitespace-nowrap">
         Testimonials
       </div>
       <div className="text-neutral-500 text-center mb-6 text-lg self-stretch w-full mt-11 mx-3.5 max-md:max-w-full max-md:mr-2.5 max-md:mt-10">
@@ -15,29 +15,29 @@ function Testomonials() {
         consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur
         adipiscing elit.
       </div>
-      <div className='flex flex-col  justify-center border-4 border-black '>
+      <div className='flex justify-center max-md:flex-wrap '>
     
-      <div className=" border-2 border-black w-full flex flex-col  max-md:max-w-full max-md:mt-2">
-        <div className=" my-4 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-center w-[40%] max-md:w-full max-md:ml-0">
+      <div className="flex flex-col w-full max-md:max-w-full max-md:mt-2">
+        <div className="flex my-4 max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-center w-[45%] max-md:w-full max-md:ml-0">
             <Image
               src={user1}
               
               className="aspect-[1.06] object-contain object-center w-full overflow-hidden grow"
             />
           </div>
-          <div className="flex flex-col items-stretch w-[43%]  max-md:w-full max-md:ml-0">
-            <div className="bg-stone-800 flex grow flex-col w-full pl-6 pr-3 py-7 items-start max-md:max-w-full max-md:pl-5">
-              <div className="text-white text-5xl font-semibold leading-10 self-stretch">
+          <div className="flex flex-col items-stretch w-[55%]  max-md:w-full max-md:ml-0">
+            <div className="flex flex-col items-start w-full pl-6 pr-3 bg-stone-800 grow py-7 max-md:max-w-full max-md:pl-5">
+              <div className="self-stretch text-5xl font-semibold leading-10 text-white">
                 “Amazing Team with Lorem Ipsum”
               </div>
-              <div className="text-white text-base font-light self-stretch mt-8">
+              <div className="self-stretch mt-8 text-base font-light text-white">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
                 ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
                 dolor sit amet,
               </div>
               <div className="flex gap-2 ml-2.5 mt-14 items-start max-md:mt-10">
-                <div className="text-white text-right text-base font-medium grow whitespace-nowrap">
+                <div className="text-base font-medium text-right text-white grow whitespace-nowrap">
                   See full review{" "}
                 </div>
                 <img
@@ -50,20 +50,20 @@ function Testomonials() {
           </div>
           
         </div>
-        <div className="gap-0  my-4 flex max-md:flex-col-reverse max-md:items-stretch max-md:gap-0">
+        <div className="flex gap-0 my-4 max-md:flex-col-reverse max-md:items-stretch max-md:gap-0">
          
-          <div className="flex flex-col items-stretch w-[43%]  max-md:w-full max-md:ml-0">
-            <div className="bg-stone-800 flex grow flex-col w-full pl-6 pr-3 py-7 items-start max-md:max-w-full max-md:pl-5">
-              <div className="text-white text-5xl font-semibold leading-10 self-stretch">
+          <div className="flex flex-col items-stretch w-[55%]  max-md:w-full max-md:ml-0">
+            <div className="flex flex-col items-start w-full pl-6 pr-3 bg-stone-800 grow py-7 max-md:max-w-full max-md:pl-5">
+              <div className="self-stretch text-5xl font-semibold leading-10 text-white">
                 “Amazing Team with Lorem Ipsum”
               </div>
-              <div className="text-white text-base font-light self-stretch mt-8">
+              <div className="self-stretch mt-8 text-base font-light text-white">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
                 ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
                 dolor sit amet,
               </div>
               <div className="flex gap-2 ml-2.5 mt-14 items-start max-md:mt-10">
-                <div className="text-white text-right text-base font-medium grow whitespace-nowrap">
+                <div className="text-base font-medium text-right text-white grow whitespace-nowrap">
                   See full review{" "}
                 </div>
                 <img
@@ -75,7 +75,7 @@ function Testomonials() {
             </div>
             
           </div>
-          <div className="flex flex-col items-center w-[40%] max-md:w-full max-md:ml-0">
+          <div className="flex flex-col items-center w-[45%] max-md:w-full max-md:ml-0">
             <Image
               src={user2}
               
@@ -85,26 +85,26 @@ function Testomonials() {
           
         </div>
       </div>
-      <div className="gap-0 flex-col my-4 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-center w-[40%] max-md:w-full max-md:ml-0">
+      <div className="flex flex-col gap-0 m-4 max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-center w-[80%] h-[48%]  max-md:w-full max-md:ml-0">
             <Image
               src={user3}
               
               className="aspect-[1.06] object-contain object-center w-full overflow-hidden grow"
             />
           </div>
-          <div className="flex flex-col items-stretch w-[43%]  max-md:w-full max-md:ml-0">
-            <div className="bg-stone-800 flex grow flex-col w-full pl-6 pr-3 py-7 items-start max-md:max-w-full max-md:pl-5">
-              <div className="text-white text-5xl font-semibold leading-10 self-stretch">
+          <div className="flex flex-col items-stretch w-[80%] h-[52%]  max-md:w-full max-md:ml-0">
+            <div className="flex flex-col items-start w-full pl-6 pr-3 bg-stone-800 grow py-7 max-md:max-w-full max-md:pl-5">
+              <div className="self-stretch text-5xl font-semibold leading-10 text-white">
                 “Amazing Team with Lorem Ipsum”
               </div>
-              <div className="text-white text-base font-light self-stretch mt-8">
+              <div className="self-stretch mt-8 text-base font-light text-white">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
                 ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
                 dolor sit amet,
               </div>
               <div className="flex gap-2 ml-2.5 mt-14 items-start max-md:mt-10">
-                <div className="text-white text-right text-base font-medium grow whitespace-nowrap">
+                <div className="text-base font-medium text-right text-white grow whitespace-nowrap">
                   See full review{" "}
                 </div>
                 <img
