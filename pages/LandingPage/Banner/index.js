2490876ca2aa68e55d358c78b5cca27fd1198cb3
jyothiswaroop077 <swaroopvallamkonda@nexocide.com>
2023-12-31 02:09:04 +0530

    animations made

--- pages/LandingPage/Banner/index.js
+++ pages/LandingPage/Banner/index.js
@@ -2,8 +2,17 @@ import FilterComponent from "@/pages/MediaListing/FilterComponent/FilterComponen
 import mainlogo from '@/pages/assets/mainlogo.png'
 import Image from 'next/image' ;
 import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import Reveal from "@/Components/RevealAnimation/Reveal";
 
-const optionsConfig = [
+const squareVariants = {
+    visible: { opacity: 1, scale: 4, transition: { duration: 1 } },
+    hidden: { opacity: 0, scale: 0 }
+  };
+
+const optionsConfig = [ 
     {
       key: 'state',
       label: 'Search Location',
@@ -29,10 +38,33 @@ const optionsConfig = [
     //our data items
   ];
 
+const buttonVariants = {
+    hover: {
+        scale: 1.1,
+        boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+        textShadow: "0px 0px 8px rgb(255, 255, 255)",
+        transition: {
+            duration: 0.3,
+            yoyo: Infinity
+        }
+    }
+}
+
 const Banner = () => {
+
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once: true});
+    const mainControls = useAnimation();
+
+    useEffect(() => {
+        if(isInView){
+            mainControls.start("visible");
+        }
+    }, [isInView] )
+
     return(
         <>
-        <div className="w-[100vw] rgba(35, 29, 32, 0.89) bg-opacity-25 lg:h-[140vh] sm:h-[190vh] md:h-[150vh] 2xl:h-[130vh]" style={{backgroundImage: "url('https://s3-alpha-sig.figma.com/img/8361/dc28/d4780ff4f4143e28302cc03f7a231643?Expires=1704672000&Signature=Xz6BeRagExtpnmtcKTEKZ90M7Z8CPeuMf3G~dJ~3WILoceH79CufiXkqsWCa-HXVhiHOfCtnzuequs1UiuYMlQ4juPiF-qW0odF5s5M5QoNGsL6TMiCmbrGHiFMO5Zhryx5nOzJeJJjVEYq3RGCfaSaXO~W-9eNmKcYxNWUmWEZKiCPomDw9-wjM86SK9CwU1904tjGQTBRS8tYc3XFhoCnY2iIu27LEIe67pWncsinHx1JyBn9VSBQVH5K1A~vDfZWDKdV46Otcdo-WQakXuV67xHpqYP0AjgPZ-rDYl6t76XqMtWOSom6oGrlfAsocG2JeWu9bGItHkzFCMk8quA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')", backgroundSize: "cover", width:"100%" }}>
+        <div className="opacity-0.5 w-[100vw] rgba(35, 29, 32, 0.89) backdrop-opacity-75 lg:h-[140vh] sm:h-[190vh] md:h-[150vh] 2xl:h-[130vh]" style={{backgroundImage: "url('https://s3-alpha-sig.figma.com/img/8361/dc28/d4780ff4f4143e28302cc03f7a231643?Expires=1704672000&Signature=Xz6BeRagExtpnmtcKTEKZ90M7Z8CPeuMf3G~dJ~3WILoceH79CufiXkqsWCa-HXVhiHOfCtnzuequs1UiuYMlQ4juPiF-qW0odF5s5M5QoNGsL6TMiCmbrGHiFMO5Zhryx5nOzJeJJjVEYq3RGCfaSaXO~W-9eNmKcYxNWUmWEZKiCPomDw9-wjM86SK9CwU1904tjGQTBRS8tYc3XFhoCnY2iIu27LEIe67pWncsinHx1JyBn9VSBQVH5K1A~vDfZWDKdV46Otcdo-WQakXuV67xHpqYP0AjgPZ-rDYl6t76XqMtWOSom6oGrlfAsocG2JeWu9bGItHkzFCMk8quA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')", backgroundSize: "cover", width:"100%", opacity: "100" }}>
             
             <div className="Navbar py-5 flex justify-around relative">
             <div
@@ -50,38 +82,93 @@ const Banner = () => {
             </div>
             </div>
             <div className="relative">
-            <div
+            <motion.div
+               
+                variants={{
+                    hidden: {opacity: 0, y: 15},
+                    visible: {opacity: 2, y: 0},
+                    
+                }}
+                initial="hidden"
+                animate="visible"
+                transition={{duration: 0.5, delay: 1.25}}
                 className={
                 "text-[#ffffff] text-left font-['Inter-Medium',_sans-serif] text-2xl font-normal w-[70vw] absolute left-[calc(10vw)] top-[calc(5vw)]"
                 }
             >
                 Amplify Your Brand, Captivate Your Audience -<br />
                 Your Gateway to Impactful Advertising!{" "}
-            </div>
-            <div
+            </motion.div>
+            <motion.div
+            variants={{
+                hidden: {opacity: 0, y:75},
+                visible: {opacity: 2, y: 0},
+                hover: {
+                    scale: [1,1.1,1],
+                    transition: {
+                        duration: 0.75,
+                    }
+                }
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{duration: 1.5, delay: 0.75, type: "spring"}}
+            whileHover="hover"
+            whileInView="animate"
+            viewport={{
+                once: true
+            }}
             className={
-                "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] text-[180.3414001464844px] font-bold relative left-[calc(15vw)] top-[calc(10vw)] z-10"
+                "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] md:text-[180.3414001464844px] font-bold relative left-[calc(15vw)] top-[calc(10vw)] z-10 xs:text-[100px]"
             }
             >
             OOH{" "}
-            </div>
-            <div
+            </motion.div>
+            <motion.div
+            variants={{
+                hidden: {opacity: 0, y:75},
+                visible: {opacity: 2, y: 0},
+                hover: {
+                    scale: [1,1.1,1],
+                    transition: {
+                        duration: 0.75,
+                    }
+                }
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{duration: 1, delay: 0.75}}
+            whileHover="hover"
             className={
                 "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] text-[160.8080139160156px] font-bold relative left-[calc(20vw)] top-[calc(2vw)] z-10"
             }
             >
             Awaits!{" "}
-            </div>
+            </motion.div>
             <div>
-            <div
+            <motion.div
+            variants={{
+                hidden: {opacity: 0},
+                visible: {opacity: 3},
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{duration: 1.5, delay: 0}}
             className={"w-[30vw] h-[50vh] relative top-[calc(-12vw)] bg-cover"}
             style={{backgroundImage: 'url("https://s3-alpha-sig.figma.com/img/3181/f79d/5f3ed537229f96087c4eca08efd968b9?Expires=1704672000&Signature=KwEaDsA1aQ~gsYSxmCAbkmDIfUja-Tb0ad6cSXyCK6i7WABBen8NJd69x3PocMgsr9pC07dBSK0U9dgAlkxdk~g5p0JSq-MAHRyBCRdRfZZtwJeIm9CinJmEkY0rlsf9dM1RJky7UO5K0czLcfWJXTqvxxLyeVhDiyWkWe-BB57j6VzlMCJNlKMjudxN5lndI2UBSRwyfAx9kx-oYYq270wHvlXSWs-hxMwIJTkSdGklI1N9~FvcqjvgaEfaRyZXhaV~x5VZG6CfxfSv9bzCF~eOKeHJSj9jZCmVR4widyukXvzrAfur8ua8D223QzFxyJBQs~mr8fJ-4BvvLv0fvA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")'}}
-            ></div>
+            ></motion.div>
 
-            <div
+            <motion.div
+            variants={{
+                hidden: {opacity: 0, },
+                visible: {opacity: 2},
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{duration: 1.5, delay: 0}}
             className={"w-[30vw] h-[50vh] relative left-[calc(70vw)] top-[calc(-47vw)] bg-cover"}
             style={{backgroundImage: 'url("https://s3-alpha-sig.figma.com/img/fa98/8e09/1a4b35e0c0523eeeb654c75c59849568?Expires=1704672000&Signature=jH4ZJoEO-w3ZgNO5n7iZIQ51Fi9wOLyNDc8OYfvrr9kuk7oMqocOZKOudYDFT~8GMfeOtEjj3Fwnd9blDR9X0ek9UTqHC~ziHwt5O2xW2fzFQM73JfIWDBbUE8ffDz~XpphC-oHGQu6SF1mROzTWbpqZZZgyl63J~IY7DH~lesjA6~KESk1cSOD4-SkaTJsoiznGk6wkNuX5WvmF7vdlLspnsd3VyVryMCi-A~uEAcC2zLsXIHp692wZ-5JgnskR7szkp5bQp4l-0Vp2jLCKomYrPcxCo0gSKRWbVngx02T4XEVmswZL7ZkKjli5IggW5kpja9fVldGHV~cLI9KEKQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")'}}
-            ></div>
+            ></motion.div>
             </div>
             <div className="flex-col items-center justify-center">
             <div className={"self-center left-[calc(5vw)] relative top-[calc(-45vw)] z-10  "}>
@@ -89,7 +176,7 @@ const Banner = () => {
             </div>
 
             <div className="button self-center left-[calc(40vw)] relative top-[calc(-45vw)]">
-                <button className="bg-[#ffffff] rounded-[36px] w-[140px] h-[52px]  text-[#0e0d0d] text-center text-[18.40671920776367px] font-semibold font-['Figtree-SemiBold',_sans-serif]">Explore Now</button>
+                <motion.button variants={buttonVariants} whileHover="hover" className="bg-[#ffffff] rounded-[36px] w-[140px] h-[52px]  text-[#0e0d0d] text-center text-[18.40671920776367px] font-semibold font-['Figtree-SemiBold',_sans-serif]">Explore Now</motion.button>
                 <div className="relative left-[calc(7vw)] top-[calc(-1vw)]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="42" height="46" viewBox="0 0 62 66" fill="none">
                     <g filter="url(#filter0_d_844_738)">
