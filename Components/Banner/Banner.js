4c5b31adf5d78bb5ffd87fbc08aa48d9f87fe4cc
jyothiswaroop077 <swaroopvallamkonda@nexocide.com>
2024-01-07 21:05:09 +0530

    homepage aligned

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -69,21 +69,21 @@ const Banner = () => {
 
     return(
         <>
-        <div className="opacity-0.5 w-[100vw] rgba(35, 29, 32, 0.89) backdrop-opacity-75 lg:h-[140vh] xs:h-[140vh] sm:h-[100vh] md:h-[150vh] 2xl:h-[130vh]" 
+        <div className="opacity-0.5 w-[100vw] rgba(35, 29, 32, 0.89) backdrop-opacity-75 lg:h-[140vh] xs:h-[160vh] sm:h-[140vh] md:h-[150vh] 2xl:h-[130vh]" 
         style={{backgroundImage: "url('https://s3-alpha-sig.figma.com/img/8361/dc28/d4780ff4f4143e28302cc03f7a231643?Expires=1704672000&Signature=Xz6BeRagExtpnmtcKTEKZ90M7Z8CPeuMf3G~dJ~3WILoceH79CufiXkqsWCa-HXVhiHOfCtnzuequs1UiuYMlQ4juPiF-qW0odF5s5M5QoNGsL6TMiCmbrGHiFMO5Zhryx5nOzJeJJjVEYq3RGCfaSaXO~W-9eNmKcYxNWUmWEZKiCPomDw9-wjM86SK9CwU1904tjGQTBRS8tYc3XFhoCnY2iIu27LEIe67pWncsinHx1JyBn9VSBQVH5K1A~vDfZWDKdV46Otcdo-WQakXuV67xHpqYP0AjgPZ-rDYl6t76XqMtWOSom6oGrlfAsocG2JeWu9bGItHkzFCMk8quA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')", backgroundSize: "cover", width:"100%"}}>
         <div className="absolute inset-0 backdrop-blur-sm">
             
             <div className="Navbar py-5 flex justify-around relative">
             <Link href="/"
-                className={"bg-[#ffffff] w-[149px] h-[37px] relative "}
+                className={"bg-[#ffffff] xs:w-[90px] sm:w-[149px] xs:h-[32px] sm:h-[37px] relative "}
                 >
                     <Image 
-                    className="w-[136.7px] h-8 relative"  height={32} width={136.7}
+                    className="xs: w-[80px] sm:w-[136.7px] h-8 relative"  height={32} width={136.7}
                     src='/assets/mainlogo.png' alt="main logo" />
                 </Link>  
-            <div className="links flex justify-between">
-                <Link href="/Marketplace" className="mx-3 text-[#d9d9d9] text-center font-['Figtree-Medium',_sans-serif] text-base leading-[101%] font-medium relative w-[129px]">Marketplace</Link>
-                <Link href="/Partner" className="mx-3 text-[#d9d9d9] text-center font-['Figtree-Medium',_sans-serif] text-base leading-[101%] font-medium relative w-[129px]">Become a partner</Link>
+            <div className="links flex  justify-between">
+                <Link href="/Marketplace" className="xs:mr-4 sm:mx-3 text-[#d9d9d9] text-center font-['Figtree-Medium',_sans-serif] xs:text-[16px] sm:text-base leading-[101%] font-medium relative xs:w-[62px] sm:w-[129px]">Marketplace</Link>
+                <Link href="/Partner" className="xs:ml-4 sm:mx-3 text-[#d9d9d9] text-center font-['Figtree-Medium',_sans-serif] xs:text-[16px] sm:text-base leading-[101%] font-medium relativew xs:w-[118px] sm:w-[129px]">Become a partner</Link>
             </div>
             </div>
             
@@ -99,7 +99,7 @@ const Banner = () => {
                 animate="visible"
                 transition={{duration: 0.5, delay: 1.25}}
                 className={
-                "text-[#ffffff] text-left font-['Inter-Medium',_sans-serif] text-2xl font-normal w-[70vw] absolute left-[calc(10vw)] top-[calc(5vw)]"
+                "text-[#ffffff] text-left font-['Inter-Medium',_sans-serif] xs:text-[16px] sm:text-2xl font-normal w-[70vw] absolute left-[calc(10vw)] top-[calc(5vh)]"
                 }
             >
                 Amplify Your Brand, Captivate Your Audience -<br />
@@ -125,7 +125,7 @@ const Banner = () => {
                 once: true
             }}
             className={
-                "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] md:text-[180.3414001464844px] font-bold relative left-[calc(25vw)] top-[calc(40vw)] md:left-[calc(15vw)] md:top-[calc(10vw)] z-10 xs:text-[50px]"
+                "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] md:text-[180.3414001464844px] font-bold relative  left-[calc(25vw)] top-[calc(20vh)] md:left-[calc(15vw)] md:top-[calc(14vh)] z-10 xs:text-[50px]"
             }
             >
             OOH{" "}
@@ -146,7 +146,7 @@ const Banner = () => {
             transition={{duration: 1, delay: 0.75}}
             whileHover="hover"
             className={
-                "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] md:text-[160.8080139160156px] font-bold relative left-[calc(30vw)] top-[calc(34vw)] md:left-[calc(20vw)] md:top-[calc(2vw)] z-10 xs:text-[50px]"
+                "text-[#ffffff] text-left font-['PlayfairDisplay-Bold',_sans-serif] md:text-[160.8080139160156px] font-bold relative  left-[calc(30vw)] top-[calc(19vh)] md:left-[calc(20vw)] md:top-[calc(5vh)] z-10 xs:text-[50px]"
             }
             >
             Awaits!{" "}
@@ -160,7 +160,7 @@ const Banner = () => {
             initial="hidden"
             animate="visible"
             transition={{duration: 1.5, delay: 0}}
-            className={"w-[30vw] h-[25vh] md:h-[50vh] relative top-[calc(8vw)] md:top-[calc(-12vw)] bg-cover"}
+            className={"w-[30vw] h-[25vh] md:h-[50vh] relative top-[calc(8vw)] top-[calc(1vh)] md:top-[calc(-12vh)] bg-cover"}
             style={{backgroundImage: 'url("https://s3-alpha-sig.figma.com/img/3181/f79d/5f3ed537229f96087c4eca08efd968b9?Expires=1704672000&Signature=KwEaDsA1aQ~gsYSxmCAbkmDIfUja-Tb0ad6cSXyCK6i7WABBen8NJd69x3PocMgsr9pC07dBSK0U9dgAlkxdk~g5p0JSq-MAHRyBCRdRfZZtwJeIm9CinJmEkY0rlsf9dM1RJky7UO5K0czLcfWJXTqvxxLyeVhDiyWkWe-BB57j6VzlMCJNlKMjudxN5lndI2UBSRwyfAx9kx-oYYq270wHvlXSWs-hxMwIJTkSdGklI1N9~FvcqjvgaEfaRyZXhaV~x5VZG6CfxfSv9bzCF~eOKeHJSj9jZCmVR4widyukXvzrAfur8ua8D223QzFxyJBQs~mr8fJ-4BvvLv0fvA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")'}}
             ></motion.div>
 
@@ -172,17 +172,17 @@ const Banner = () => {
             initial="hidden"
             animate="visible"
             transition={{duration: 1.5, delay: 0}}
-            className={"w-[30vw] h-[25vh] md:h-[50vh] relative left-[calc(70vw)]  top-[calc(-47vw)] bg-cover"}
+            className={"w-[30vw] h-[25vh] md:h-[50vh] relative left-[calc(70vw)]  top-[calc(-30vh)]  md:top-[calc(-85vh)] bg-cover"}
             style={{backgroundImage: 'url("https://s3-alpha-sig.figma.com/img/fa98/8e09/1a4b35e0c0523eeeb654c75c59849568?Expires=1704672000&Signature=jH4ZJoEO-w3ZgNO5n7iZIQ51Fi9wOLyNDc8OYfvrr9kuk7oMqocOZKOudYDFT~8GMfeOtEjj3Fwnd9blDR9X0ek9UTqHC~ziHwt5O2xW2fzFQM73JfIWDBbUE8ffDz~XpphC-oHGQu6SF1mROzTWbpqZZZgyl63J~IY7DH~lesjA6~KESk1cSOD4-SkaTJsoiznGk6wkNuX5WvmF7vdlLspnsd3VyVryMCi-A~uEAcC2zLsXIHp692wZ-5JgnskR7szkp5bQp4l-0Vp2jLCKomYrPcxCo0gSKRWbVngx02T4XEVmswZL7ZkKjli5IggW5kpja9fVldGHV~cLI9KEKQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")'}}
             ></motion.div>
             </div>
             <div className="flex-col items-center justify-center">
 
-            <div className={"self-center  relative top-[calc(-45vw)] z-10  "}>
+            <div className={"3xl:left-[calc(8vw)] relative top-[calc(-15vh)] md:top-[calc(-65vh)] z-10  "}>
                 <FilterComponent data={data} optionsConfig={optionsConfig} />
             </div>
 
-            <div className="button self-center left-[calc(45vw)] relative top-[calc(-45vw)]">
+            <div className="button self-center left-[calc(35vw)] sm:left-[calc(45vw)] relative top-[calc(-10vh)] md:top-[calc(-65vh)]">
                 <motion.button onClick={onClickExplore} variants={buttonVariants} whileHover="hover" className="bg-[#ffffff] rounded-[36px] w-[140px] h-[52px]  
                 text-[#0e0d0d] text-center text-[18.40671920776367px] font-semibold font-['Figtree-SemiBold',_sans-serif]">Explore Now</motion.button>
 
