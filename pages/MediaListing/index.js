1f1a38fd9dd9ca722f6e649700b0158efb37dbc4
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-01-08 22:49:12 +0530

    fixed MediaListing grids

--- pages/MediaListing/index.js
+++ pages/MediaListing/index.js
@@ -267,7 +267,7 @@ const MediaListingPage = () => {
       
       <div className="px-5 md:px-8 xl:px-8 pb-5">
         <div className='flex justify-center'>
-        <ul className='px-8 grid grid-cols-1 sm:grid-cols-2 sm:px-4 lg:grid-cols-3 2xl:grid-cols-4   max-h-[90vh] overflow-y-auto w-full overscroll-y-auto mb-4 '>
+        <ul className='px-8 grid grid-cols-1 sm:grid-cols-2 sm:px-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-5 4xl:grid-cols-6  max-h-[90vh] overflow-y-auto w-full overscroll-y-auto mb-4 '>
           {
             searchData.map((curElem, ind) => {
               return <BlogCard key={ind} data={curElem} onClickWishlistButton = {onClickWishlistButton}/>
