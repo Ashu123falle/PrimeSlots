5ca86541b0dbef017b823521c3874dd807d46040
Umesh <umeshkhantwal@nexocide.com>
2024-01-10 02:42:03 -0800

    ContactUs page components

---
+++ pages/ContactUs/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContactUsnavbar from '@/Components/ContactUsnavbar/ContactUsnavbar'
+import Footer from '@/Components/Footer/Footer'
+import LetsTalk from '@/Components/LetsTalk/LetsTalk'
+import StayLoop from '@/Components/StayLoop/StayLoop'
+
+const ContactUs = () => {
+  return (
+    <>
+      <ContactUsnavbar />
+      <LetsTalk />
+      <StayLoop />
+      <Footer />
+    </>
+  )
+}
+
+export default ContactUs
