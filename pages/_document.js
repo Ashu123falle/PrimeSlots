a62b96523bff11aacb626f812496d524f1a36d63
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-01-11 20:47:29 +0530

    created Career Openings section in Jobs page.

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,14 @@ export default function Document() {
         <Main />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
 
-  <link rel="preconnect" href="https://fonts.gstatic.com"/>
-  <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700;800;900&display=swap"
+           rel="stylesheet" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" 
+          rel="stylesheet"/>
 
 
           
