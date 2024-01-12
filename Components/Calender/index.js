8446cdd2a3575f078e3dd374ca010e4ba8588a52
Eswar <eswarnarayana797@gmail.com>
2024-01-12 19:53:12 +0530

    Created Calender

--- Components/Calender/index.js
+++ Components/Calender/index.js
@@ -181,7 +181,7 @@ const CalendarPage = () => {
             color: white;
             padding: 10px 20px;
             border: none;
-            border-radius: 5px;
+            border-radius: 4px;
             cursor: pointer;
           }
         `}
